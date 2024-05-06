f3579f70e3e2fae859209b558821bc8e3278db8a
Fabio Pedrini <fabio.pedrini3@studio.unibo.it>
2024-05-06 11:32:59 +0200

    Package java and class creation

---
+++ src/main/java/Pedrini.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class Pedrini {
+
+    public static void main(String[] args) {
+        helloWorld();
+    }
+
+    public static void helloWorld(){
+        System.out.println("Hello World");
+    }
+}
